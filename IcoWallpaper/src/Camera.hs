6586d48546ab090fb07bc041b166fc3a95dba408
Miksu Rankaviita <8172649+MiksuR@users.noreply.github.com>
2019-11-26 19:58:18 +0200

    Change type definitions in Camera.hs to more sensible ones.

--- IcoWallpaper/src/Camera.hs
+++ IcoWallpaper/src/Camera.hs
@@ -1,71 +1,49 @@
 module Camera (
     Scene,
-    Camera,
-    renderScene,
-    NormalCam(..),
-    BlurredCam(..),
+    Camera(..),
+    renderNormal,
+    renderBlurred
 ) where
 
 import Graphics.Gloss hiding (Line)
 import LinearEqs
 import Shapes
 
+-- Type definitions
 type Scene = [Shape]
-
--- Maybe redifine:
--- data Camera = Camera {pos, dir, width, dist}
--- data Renderer = Renderer {renderScene :: (Camera -> (Int, Int) -> Scene -> Picture)}
-
--- Define the Camera class
-class Camera camera where
-    position :: camera -> Vector3
-    direction :: camera -> Vector3
-    pictureWidth :: camera -> Float
-    distanceToScreen :: camera -> Float
-    renderScene :: camera -> (Int, Int) -> Scene -> Picture
-
--- Define Camera types
-data NormalCam = NormalCam {
+data Camera = Camera {
     pos :: Vector3,
     dir :: Vector3,
     width :: Float,
     dist :: Float}
-instance Camera NormalCam where
-    position = pos
-    direction = dir
-    pictureWidth = width
-    distanceToScreen = dist
-    renderScene c screen scen = pictures . concat $ map shapeToLines scen
+type Renderer = Camera -> (Int, Int) -> Scene -> Picture
+
+-- Renderers
+renderNormal :: Renderer
+renderNormal c screen scen = pictures . concat $ map shapeToLines scen
         where shapeToLines = map $ line . (projectEdge c screen)
 
-data BlurredCam = BlurredCam {
-    bPos :: Vector3,
-    bDir :: Vector3,
-    bWidth :: Float,
-    bDist :: Float,
-    fDist :: Float}
-instance Camera BlurredCam where
-    position = bPos
-    direction = bDir
-    pictureWidth = bWidth
-    distanceToScreen = bDist
-    renderScene c screen scen = pointsToPicture $ map shapeToPoints scen
+renderBlurred :: Renderer
+renderBlurred c screen scen = pointsToPicture $ map shapeToPoints scen
         where shapeToPoints = undefined
 
--- Camera helper functions
-projectPoint :: (Camera c) => c -> (Int, Int) -> Vector3 -> Point
+renderBlurredDistort :: Renderer
+renderBlurredDistort = undefined
+
+-- Renderer helper functions
+projectPoint :: Camera -> (Int, Int) -> Vector3 -> Point
 projectPoint camera screen point = scale $ planeCoordinates plane (planeBasis plane) intersection
     where
-        center = (position camera) `vAdd` ((distanceToScreen camera) `sProd` (direction camera))
-        plane = Plane center (direction camera)
-        intersection = linePlaneIntrsct (Line (position camera) point) plane
+        center = (pos camera) `vAdd` ((dist camera) `sProd` (dir camera))
+        plane = Plane center (dir camera)
+        intersection = linePlaneIntrsct (Line (pos camera) point) plane
         scale (a, b) = (a*ratio, b*ratio)
-        ratio = (fromIntegral $ fst screen) / (pictureWidth camera)
+        ratio = (fromIntegral $ fst screen) / (width camera)
 
-projectEdge :: NormalCam -> (Int, Int) -> Edge -> Path
+projectEdge :: Camera -> (Int, Int) -> Edge -> Path
 projectEdge camera screen (Edge a b) = map (projectPoint camera screen) [a, b]
 
-projectBlurredEdge :: BlurredCam -> (Int, Int) -> Edge -> [Point]
+projectBlurredEdge :: Camera -> (Int, Int) -> Edge -> [Point]
 projectBlurredEdge camera screen (Edge a b) = undefined
 
 pointsToPicture :: [Point] -> Picture -- Change to bitmap IG
--- IcoWallpaper/src/Main.hs
+++ IcoWallpaper/src/Main.hs
@@ -13,9 +13,10 @@ main = do
 
 frameAt :: (Int, Int) -> Float -> Picture
 frameAt size time = renderScene camera size (scene time)
+    where renderScene = renderNormal -- Change this to one of {renderNormal, renderBlurred, renderBlurredDistort}
 
 scene :: Float -> Scene
 scene time = [rotateShape 0 0 time uprightIco]
 
-camera :: NormalCam
-camera = NormalCam (Vector3 5 0 0) (Vector3 (-1) 0 0) 0.5 0.25
+camera :: Camera
+camera = Camera (Vector3 5 0 0) (Vector3 (-1) 0 0) 0.5 0.25
